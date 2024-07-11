e98468418b33a26d54987c392835d90236518e5e
batina7777 <batina.adams@gmail.com>
2024-07-11 16:19:50 -0600

    Functions stepDefinitions is updated2

--- src/test/java/com/cydeo/step_definitions/FunctionsCompanyStructureStepDef.java
+++ src/test/java/com/cydeo/step_definitions/FunctionsCompanyStructureStepDef.java
@@ -11,6 +11,26 @@ import org.junit.Assert;
 
 public class FunctionsCompanyStructureStepDef {
 
+    LoginPage loginPage = new LoginPage();
+
+    @Given("user is on the login page")
+    public void userIsOnTheLoginPage() {
+        System.out.println("DONE IN HOOKS");
+        //Driver.getDriver().get(ConfigurationReader.getProperty("url"));
+
+    }
+
+
+    @When("user logs in as {string}")
+    public void user_logs_in_as(String userType) {
+        loginPage.login(userType);
+    }
+
+    @Then("user should be able to see {string} as page title")
+    public void userShouldLandOnHomePageWith(String title) {
+        BrowserUtils.verifyTitleContains(title);
+    }
+
     FunctionsInCompanyStructurePage functionsInCompanyStructurePage = new FunctionsInCompanyStructurePage();
 
 
@@ -35,6 +55,11 @@ public class FunctionsCompanyStructureStepDef {
 
     @Then("user should see the {string}")
     public void userShouldSeeThe(String arg0) {
+        Assert.assertTrue(functionsInCompanyStructurePage.companyStructureTitle.isDisplayed());
+    }
+
+    @Then("user should see the {string} Employees page")
+    public void userShouldSeeTheEmployeesPage( String arg1) {
         Assert.assertTrue(functionsInCompanyStructurePage.companyStructureTitle.isDisplayed());
     }
 }
