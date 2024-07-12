e26489433296adfb9ec74b43818be50935dafc2f
batina7777 <batina.adams@gmail.com>
2024-07-12 11:27:48 -0600

    UploadFilesAndPicturesAsMessagesPage is added

---
+++ src/test/java/com/cydeo/pages/UploadFilesAndPicturesAsMessagesPage.java
@@ -0,0 +1,29 @@
+package com.cydeo.pages;
+
+import com.cydeo.utilities.Driver;
+import org.openqa.selenium.WebElement;
+import org.openqa.selenium.support.FindBy;
+import org.openqa.selenium.support.PageFactory;
+
+public class UploadFilesAndPicturesAsMessagesPage {
+
+
+    public UploadFilesAndPicturesAsMessagesPage(){
+        PageFactory.initElements(Driver.getDriver(), this);
+    }
+
+    @FindBy(xpath = "(//span[.='More'])[1]")
+    public WebElement moreDropdown;
+
+    @FindBy (xpath = "(//span[.='File'])[1]")
+    public WebElement fileDropdown;
+
+    @FindBy (xpath = "(//span[.='Upload files and images'])[1]")
+    public WebElement uploadFilesAndImages;
+
+    @FindBy (xpath = "(//span[@id='check-in-text-n3654'])[1]")
+    public WebElement insertInText;
+
+    @FindBy(xpath = "(//button[@id='blog-submit-button-cancel'])[1]")
+    public WebElement cancelButton;
+}
