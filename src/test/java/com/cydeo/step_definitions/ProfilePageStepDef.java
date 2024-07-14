6b6027562ec10b37661ae01378c3632a8b95e64a
tuckstpltn <tuckerstapleton@gmail.com>
2024-07-13 22:15:31 -0600

    Created Profile Page Step Definitions

---
+++ src/test/java/com/cydeo/step_definitions/ProfilePageStepDef.java
@@ -0,0 +1,27 @@
+package com.cydeo.step_definitions;
+
+import io.cucumber.java.en.Then;
+import io.cucumber.java.en.When;
+
+public class ProfilePageStepDef {
+
+    @When("user clicks username to reveal dropdown")
+    public void user_clicks_username_to_reveal_dropdown() {
+        // Write code here that turns the phrase above into concrete actions
+        throw new io.cucumber.java.PendingException();
+    }
+
+    @When("user clicks My Profile")
+    public void user_clicks_my_profile() {
+        // Write code here that turns the phrase above into concrete actions
+        throw new io.cucumber.java.PendingException();
+    }
+
+    @Then("user is on My Profile Page")
+    public void user_is_on_my_profile_page() {
+        // Write code here that turns the phrase above into concrete actions
+        throw new io.cucumber.java.PendingException();
+    }
+
+
+}
