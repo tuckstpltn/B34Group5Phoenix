ea7a1fd9ce8a160e75d474efcf6cfe54457d3a52
tuckstpltn <tuckerstapleton@gmail.com>
2024-07-15 22:00:23 -0600

    Created Activity Stream feature and step definition classes

---
+++ src/test/java/com/cydeo/step_definitions/ActivityStreamStepDef.java
@@ -0,0 +1,21 @@
+package com.cydeo.step_definitions;
+
+import io.cucumber.java.en.Then;
+
+public class ActivityStreamStepDef {
+
+    /*
+    Activity Stream Elements located under HomePage
+     */
+
+    @Then("user clicks {string} to see {string} {string} {string} {string}")
+    public void user_clicks_to_see(String string, String string2, String string3, String string4, String string5) {
+
+    }
+
+    @Then("user should see {string} {string} {string} {string} options")
+    public void user_should_see_options(String string, String string2, String string3, String string4) {
+
+    }
+
+}
